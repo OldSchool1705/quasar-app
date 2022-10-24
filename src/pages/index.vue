97ef45ef497931bf858f3667d75ed019df2ef1f0
Artem.Zubkov <oldschool1705@gmail.com>
2022-10-24 22:49:02 +0300

    quasar add project

---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div><h1>111111111</h1></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
